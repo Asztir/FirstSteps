65ec3a3ea3056d0cfc1de5f05e2a21fb8c3d87fd
asztir <asztir@knycz.net>
2022-08-12 13:03:17 +0200

    MaxOfThree

--- src/main/java/Petle/MaxOfThree.java
+++ src/main/java/Petle/MaxOfThree.java
@@ -1,2 +1,29 @@
-package MaxOfThree;public class MaxOfThree {
+package Petle;
+
+import java.util.Scanner;
+
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę naturalną a");
+        int aNumber = scanner.nextInt();
+        System.out.println("Podaj liczbę naturalną b");
+        int bNumber = scanner.nextInt();
+        System.out.println("Podaj liczbę naturalną c");
+        int cNumber = scanner.nextInt();
+        System.out.println(numbers(aNumber, bNumber, cNumber));
+    }
+    public static int numbers(int aNumber, int bNumber, int cNumber){
+        if (aNumber >= bNumber && aNumber >= cNumber) {
+            return aNumber;
+        } else if (bNumber >= aNumber && bNumber >= cNumber) {
+            return bNumber;
+        } else if (cNumber >= aNumber && cNumber >= bNumber) {
+            return cNumber;
+        } else if (aNumber == bNumber && aNumber == cNumber) {
+            return aNumber;
+        }
+        return aNumber;
+    }
 }
+
--- src/main/java/Petle/ThreeNumbers.java
+++ src/main/java/Petle/ThreeNumbers.java
@@ -1,2 +1,16 @@
-package Petle;public class ThreeNumbers {
+package Petle;
+
+public class ThreeNumbers {
+    public static int numbers(int aNumber, int bNumber, int cNumber){
+    if (aNumber >= bNumber && aNumber >= cNumber) {
+        return aNumber;
+    } else if (bNumber >= aNumber && bNumber >= cNumber) {
+        return bNumber;
+    } else if (cNumber >= aNumber && cNumber >= bNumber) {
+        return cNumber;
+    } else if (aNumber == bNumber && aNumber == cNumber) {
+        return aNumber;
+    }
+        return aNumber;
+}
 }
